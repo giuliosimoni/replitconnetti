d05f8f0f11f0a1f221050ddcbc8fc9d598f9afd4
simonigiulio98 <95945460+giuliosimoni@users.noreply.github.com>
2022-04-06 09:14:53 +0000

    RECUPERO

---
+++ src/ESERCIZI RECUPERO/PYTHON 2/ES1.py
@@ -0,0 +1,5 @@
+giorni=int(input('n_giorni:'))
+ore=int(input('n_ore:'))
+minuti=int(input('n_minuti:'))
+secondi=giorni*86400+ore*3600+minuti*60
+print('numero totale di secondi:',secondi)
---
+++ src/ESERCIZI RECUPERO/PYTHON 2/ES3.py
@@ -0,0 +1,17 @@
+a=float(input())
+b=float(input())
+c=float(input())
+ 
+def minimo(x,y):
+  if x < y:
+    return x
+  else:
+    return y
+ 
+def minore(i,j,k):
+  n1=minimo(i,j)
+  n2=minimo(i,k)
+  smaller=minimo(n1,n2)
+  return smaller
+  
+print(minore(a,b,c))
